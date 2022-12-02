a2fa569be79df344d8cea15b2a1fe30890048228
Eric <erk91495@gmail.com>
2022-12-02 14:19:03 -0500

    Added some DB stubbs and code to play around with concurrency

---
+++ Bl_Orders_2023.Core/Data/BLOrdersDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bl_Orders_2023.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bl_Orders_2023.Core.Data;
+public class BLOrdersDBContext : DbContext
+{
+    public BLOrdersDBContext (DbContextOptions<BLOrdersDBContext> options) : base(options)
+    {
+
+    }
+    public DbSet<InventoryItem> Inventory
+    {
+        get; set;
+    }
+}
---
+++ Bl_Orders_2023.Core/Models/InventoryItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bl_Orders_2023.Core.Models;
+
+[Table("tbl_Inventory_(Boxed)")]
+public class InventoryItem
+{
+    [Key]
+    [Required]
+    public int ProductID
+    {
+        get; set;
+    }
+
+    [Required(AllowEmptyStrings=false)]
+    [MaxLength(30)]
+    public string ProductName
+    {
+        get; set;
+    }
+
+    [Required]
+    [ConcurrencyCheck]
+    public short QuantityOnHand
+    {
+        get; set;
+    }
+
+    [Required]
+    public short AdjustmentQuantity
+    {
+        get; set;
+    }
+
+    public short SortIndex
+    {
+        get; set;
+    }
+
+}
