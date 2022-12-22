fea7b48bc8045b19035dc278f34016826a0100f3
Eric <erk91495@gmail.com>
2022-12-22 15:44:48 -0500

    Fixed Mailto command for email link

--- BlOrders2023/Views/OrdersPage.xaml.cs
+++ BlOrders2023/Views/OrdersPage.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using BlOrders2023.Contracts.Services;
 using BlOrders2023.Helpers;
+using BlOrders2023.Models;
 using BlOrders2023.Services;
 using BlOrders2023.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System.Diagnostics;
 using Windows.ApplicationModel.Email;
+using Windows.System;
 
 namespace BlOrders2023.Views;
 
@@ -51,22 +54,24 @@ public sealed partial class OrdersPage : Page
         //    };
 
         //    if (!string.IsNullOrEmpty(ViewModel.SelectedOrder.Customer.Email))
+        //    if (!string.IsNullOrEmpty(ViewModel.SelectedOrder.Customer.Email))
         //    {
         //        var emailRecipient = new EmailRecipient(ViewModel.SelectedOrder.Customer.Email);
         //        emailMessage.To.Add(emailRecipient);
         //    }
         //    await EmailManager.ShowComposeNewEmailAsync(emailMessage);
         //}
+        await Launcher.LaunchUriAsync( new Uri(String.Format("mailto:{0}", ViewModel.SelectedOrder.Customer.Email)));
     }
 
-    private void Order_OrdersGrid_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
-    {
-        var navService = App.GetService<INavigationService>();
-        navService.NavigateTo(typeof(OrderDetailsPageViewModel).FullName!, ViewModel.SelectedOrder);
-    }
+    private void Order_OrdersGrid_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)=>
+        Frame.Navigate(typeof(OrderDetailsPage), ViewModel.SelectedOrder);
 
-    private void Order_OrdersGrid_RightTapped(object sender, Microsoft.UI.Xaml.Input.RightTappedRoutedEventArgs e)
-    {
 
-    }
+    private void OrdersGrid_RightTapped(object sender, Microsoft.UI.Xaml.Input.RightTappedRoutedEventArgs e) =>
+        ViewModel.SelectedOrder = (e.OriginalSource as FrameworkElement).DataContext as Order;
+
+
+    private void MenuFlyoutViewDetails_Click(object sender, RoutedEventArgs e) =>
+        Frame.Navigate(typeof(OrderDetailsPage), ViewModel.SelectedOrder);
 }
