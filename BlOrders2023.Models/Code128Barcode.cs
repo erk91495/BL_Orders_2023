f03e75818c2f9c6a43414341365e46d0ceb7e695
erk91459 <erk91495@gmail.com>
2023-07-13 09:38:41 -0400

    Fixed #43

--- BlOrders2023.Models/Code128Barcode.cs
+++ BlOrders2023.Models/Code128Barcode.cs
@@ -45,13 +45,30 @@ namespace BlOrders2023.Models
                         _serial = Scanline.Substring(18, 6);
                         break;
                     }
+                //B&L Ladder Barcode 605375 610 1255 23193 00424233
                 case 26:
                     {
+                        _productId = int.Parse(Scanline.Substring(6, 3));
+                        _weight = float.Parse(Scanline.Substring(9, 4)) / 100f;
+                        var year = (DateTime.Today.Year - (DateTime.Today.Year % 100)) + int.Parse(Scanline.Substring(13, 2));
+                        DateTimeOffset date = new DateTimeOffset(year, 1, 1, 0, 0, 0, TimeSpan.Zero);
+                        //subtract 1 day from Jdate to account for starting at Jan 1
+                        date = date.AddDays(int.Parse(Scanline.Substring(15, 3)) - 1);
+                        _packDate = date.Date;
+                        _serial = Scanline.Substring(18, 8);
                         break;
                     }
-                //    6053758120012552319300424233
+                //DLM Turkey & Breast Barcodes 605375 812 001255 23193 00424233
                 case 28:
                     {
+                        _productId = int.Parse(Scanline.Substring(6, 3));
+                        _weight = float.Parse(Scanline.Substring(9, 6)) / 100f;
+                        var year = (DateTime.Today.Year - (DateTime.Today.Year % 100)) + int.Parse(Scanline.Substring(15, 2));
+                        DateTimeOffset date = new DateTimeOffset(year, 1, 1, 0, 0, 0, TimeSpan.Zero);
+                        //subtract 1 day from Jdate to account for starting at Jan 1
+                        date = date.AddDays(int.Parse(Scanline.Substring(17, 3)) - 1);
+                        _packDate = date.Date;
+                        _serial = Scanline.Substring(20, 8);
                         break;
                     }
                 default:
