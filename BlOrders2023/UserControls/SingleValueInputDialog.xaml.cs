e0be508fd18418bd48736e6dfd20066b7f0f93e8
erk91459 <erk91495@gmail.com>
2023-09-12 10:24:22 -0400

    Fixes #108

--- BlOrders2023/UserControls/SingleValueInputDialog.xaml.cs
+++ BlOrders2023/UserControls/SingleValueInputDialog.xaml.cs
@@ -3,58 +3,61 @@ using Microsoft.UI.Xaml.Controls;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace BlOrders2023.UserControls
+namespace BlOrders2023.UserControls;
+
+public partial class SingleValueInputDialog : ContentDialog, INotifyPropertyChanged
 {
-    public partial class SingleValueInputDialog : ContentDialog, INotifyPropertyChanged
-    {
-        private string? _value;
-        private bool isValid = true;
+    private string? _value;
+    private bool isValid = true;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-        public string? Prompt { get; set; }
+    public string? Prompt { get; set; }
 
-        public string? Value 
+    public string? Value 
+    { 
+        get => _value; 
+        set 
         { 
-            get => _value; 
-            set 
-            { 
-                _value = value;
-                if(ValidateValue != null)
-                {
-                    isValid = ValidateValue(_value);
-                    OnPropertyChanged(nameof(notValid));
-
-                }
-            } 
-        }
-        private bool notValid { get => !isValid; }
-        public ValidationMethod? ValidateValue { get; set; }
-
-        public delegate bool ValidationMethod(string? value);
-        public SingleValueInputDialog()
-        {
-            InitializeComponent();
-            
-        }
-
-        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
-
-            if (!isValid)
+            _value = value;
+            if(ValidateValue != null)
             {
-                args.Cancel = true;
+                isValid = ValidateValue(_value);
+                OnPropertyChanged(nameof(notValid));
+
             }
+        } 
+    }
+    private bool notValid => !isValid;
+    public ValidationMethod? ValidateValue { get; set; }
 
-        }
+    public delegate bool ValidationMethod(string? value);
+    public SingleValueInputDialog()
+    {
+        InitializeComponent();
+        
+    }
 
-        /// <summary>
-        /// Raises the <see cref="PropertyChanged"/> event.
-        /// </summary>
-        /// <param name="propertyName">(optional) The name of the property that changed.</param>
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+        //If enter is used to submit the value binding does not get triggered in time.
+        if (InputBox.Text != Value)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            Value = InputBox.Text;
         }
+        if (!isValid)
+        {
+            args.Cancel = true;
+        }
+
+    }
+
+    /// <summary>
+    /// Raises the <see cref="PropertyChanged"/> event.
+    /// </summary>
+    /// <param name="propertyName">(optional) The name of the property that changed.</param>
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
