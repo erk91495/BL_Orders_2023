d68a9507c7a5ee1ea098a4bc7271bab3c8c399e5
erk91459 <erk91495@gmail.com>
2023-09-22 07:48:55 -0400

    Added settings page for #52 Fixes #140 fixes #141

--- BlOrders2023.Core/Data/IBLDatabase.cs
+++ BlOrders2023.Core/Data/IBLDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,4 +18,5 @@ public interface IBLDatabase
     public IInventoryTable Inventory { get; }
     public IAllocationTable Allocation { get; }
     public Version dbVersion { get; }
+    public DbConnection DbConnection{ get; }
 }
