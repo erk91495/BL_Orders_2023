d45cae9978473f5eb155e16000a31d0b7c42ddd3
erk91459 <erk91495@gmail.com>
2023-10-17 15:06:35 -0400

    Release Canidate 2.2.0

--- BlOrders2023/App.xaml.cs
+++ BlOrders2023/App.xaml.cs
@@ -229,7 +229,7 @@ public partial class App : Application
         {
             var db = App.GetNewDatabase();
             var DBVersion = db.dbVersion;
-            var SupportedDBVersion = new Version(1, 0, 5);
+            var SupportedDBVersion = new Version(1, 0, 6);
             if (!SupportedDBVersion.Equals(DBVersion))
             {
                 var version = Package.Current.Id.Version;
