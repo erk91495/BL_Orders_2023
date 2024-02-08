f293c49ece8a042356963a0ead2ce549e4701064
erk91459 <erk91495@gmail.com>
2024-02-08 08:23:36 -0500

    resolves #319

--- SQL Scripts/insert_tbl_ProductCategories_defaults.sql
+++ SQL Scripts/insert_tbl_ProductCategories_defaults.sql
@@ -1,6 +1,6 @@
 USE [BL_Enterprise]
 
-DELETE FROM [dbo].[tbl_ProductCategories] WHERE CategoryID <=6
+DELETE FROM [dbo].[tbl_ProductCategories] WHERE CategoryID <=17
 GO
 
 SET IDENTITY_INSERT [dbo].[tbl_ProductCategories] ON
@@ -17,7 +17,19 @@ INSERT INTO [dbo].[tbl_ProductCategories]
 	  (3, 'Roasts', 1, 520),
 	  (4, 'Bone-In Breast', 1, 530),
 	  (5, 'NON GMO Breast', 1, 540),
-	  (6, 'Breast Roast', 1, 550)
+	  (6, 'Breast Roast', 1, 550),
+	  (7, 'DLM Turkeys',1, 560),
+	  (8, 'DLM Bone-In Breast', 1, 570),
+	  (9, 'ABF Deli', 0, 600),
+	  (10, 'Commodity Deli', 0, 610),
+	  (11, 'Smoked Parts', 0, 700),
+	  (12, 'Raw Parts', 0,800),
+	  (13, 'Raw Ground', 0, 810),
+	  (14, 'Raw Breast Meat', 0, 900),
+	  (15, 'Smoked Bone-In Breast', 0, 1000),
+	  (16, 'Raw Tenders', 0, 1100),
+	  (17, 'Bulk Raw Turkey', 0, 1200)
+
 GO
 
 SET IDENTITY_INSERT [dbo].[tbl_ProductCategories] OFF
